efa6e339c1c2b19d92a6944ba6201812da7e90b1
Adrien Ventugol <ventugol.adrien@gmail.com>
2025-05-08 22:46:34 +0200

    feature: modify frontend to send llm response along with question

--- src/components/ChatCard.tsx
+++ src/components/ChatCard.tsx
@@ -1,45 +1,77 @@
-import { useState } from "react"
-import AnimatedText from "./AnimatedText"
+import { useState } from "react";
+import AnimatedText from "./AnimatedText";
 
 interface ChatCardProps {
-
-    title: string
-    placeholder?: string
-    id?:string
+  title: string;
+  placeholder?: string;
+  id?: string;
+  context?: string[];
+  clickHandler: (
+    input: string,
+    id?: string,
     context?: string[]
-    clickHandler: (input:string, id?:string, context?:string[]) => Promise<string>
+  ) => Promise<string>;
 }
 
 export const ChatCard: React.FC<ChatCardProps> = (props) => {
-    const [title, clickHandler, placeholder] = [props.title, props.clickHandler, props.placeholder]
-    const [input, setInput] = useState<string>("")
-    const [response, setResponse] = useState<string>("")
+  const [title, clickHandler, placeholder, id] = [
+    props.title,
+    props.clickHandler,
+    props.placeholder,
+    props.id,
+  ];
+  const [input, setInput] = useState<string>("");
+  const [response, setResponse] = useState<string>("");
 
-    const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value)
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+  const ask = async () => {
+    if (input) {
+      console.log("Asking...");
+      setResponse("Generating response...");
+      const llminput = input;
+      setInput("");
+      const llm_response = await clickHandler(llminput, id);
+      setResponse(llm_response);
+    } else {
+      setResponse("Please ask a question.");
     }
-    const ask = async () => {
-        if (input){
-            console.log("Asking...")
-            setResponse("Generating response...")
-            const llminput = input
-            setInput("")
-            const llm_response = await clickHandler(llminput)
-            setResponse(llm_response)
-        } else {
-            setResponse("Please ask a question.")
-        }
+  };
 
-    }
-        
-
-    return (
-        <div className="card">
-            <h2 className="cardTitle">{title}</h2>
-            {response ? <AnimatedText text={response} /> : <p className="placeholder"> {placeholder}</p>}
-            <span style={{ display: "flex", flexDirection: "row",height: "25px", width: "100%", justifyContent: "space-between", alignItems: "center", padding: "10px"}}>
-            <input style={{width: '80%', height:'100%', borderRadius: "4em", padding:"5px"}} type="text" value={input} onChange={handleInput} placeholder="Ask a question"/><button onClick={ask} >Ask</button>
-            </span>
-        </div>
-    )
-}
+  return (
+    <div className="card">
+      <h2 className="cardTitle">{title}</h2>
+      {response ? (
+        <AnimatedText text={response} />
+      ) : (
+        <p className="placeholder"> {placeholder}</p>
+      )}
+      <span
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          height: "25px",
+          width: "100%",
+          justifyContent: "space-between",
+          alignItems: "center",
+          padding: "10px",
+        }}
+      >
+        <input
+          style={{
+            width: "80%",
+            height: "100%",
+            borderRadius: "4em",
+            padding: "5px",
+          }}
+          type="text"
+          value={input}
+          onChange={handleInput}
+          placeholder="Ask a question"
+        />
+        <button onClick={ask}>Ask</button>
+      </span>
+    </div>
+  );
+};
--- src/services/askCoworkers.ts
+++ src/services/askCoworkers.ts
@@ -2,58 +2,88 @@ import { SchemaType, GoogleGenerativeAI } from "@google/generative-ai";
 import { FeedbackJSON } from "../assets/Feedback";
 import { Resume } from "../types";
 
-export const askCoworkers = async (input:string, id?:string, context?:string[]):Promise<string> => {
-    const serverURL = import.meta.env.VITE_SERVER_URL
-    const resume = import.meta.env.VITE_RESUME
-    const projects = JSON.stringify(JSON.parse(resume) as Resume["projects"])
-    const schema = {
+export const askCoworkers = async (
+  input: string,
+  id?: string,
+  context?: string[]
+): Promise<string> => {
+  const serverURL = import.meta.env.VITE_SERVER_URL;
+  const resume = import.meta.env.VITE_RESUME;
+  const projects = JSON.stringify(JSON.parse(resume) as Resume["projects"]);
+  const schema = {
+    description: "Theme of the question",
+    type: SchemaType.OBJECT,
+    properties: {
+      theme: {
+        type: SchemaType.STRING,
         description: "Theme of the question",
-        type: SchemaType.OBJECT,
-        properties: {
-            theme: {
-                type: SchemaType.STRING,
-                description: "Theme of the question",
-                nullable: false
-            }
-        },
-        required: ["theme"],
-        nullable: false
-    }
-          if (import.meta.env.VITE_GEMINI_API_KEY && input) {
-              const themeAnalyzer = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-              const model1 = themeAnalyzer.getGenerativeModel({ model: "gemini-1.5-flash",generationConfig:{responseMimeType:"application/json",responseSchema:schema}, systemInstruction: "Your role is to analyze a question given by a user about an engineer. Analyze the broad theme of the question given by the asker in term of the what abilities it concerns. Return the theme of the question in a maximum of 3 words." });
-              const themePrompt = ` a user is asking the following questions about the engineer:${input}
+        nullable: false,
+      },
+    },
+    required: ["theme"],
+    nullable: false,
+  };
+  if (import.meta.env.VITE_GEMINI_API_KEY && input) {
+    const themeAnalyzer = new GoogleGenerativeAI(
+      import.meta.env.VITE_GEMINI_API_KEY
+    );
+    const model1 = themeAnalyzer.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      generationConfig: {
+        responseMimeType: "application/json",
+        responseSchema: schema,
+      },
+      systemInstruction:
+        "Your role is to analyze a question given by a user about an engineer. Analyze the broad theme of the question given by the asker in term of the what abilities it concerns. Return the theme of the question in a maximum of 3 words.",
+    });
+    const themePrompt = ` a user is asking the following questions about the engineer:${input}
               Give the theme of the question.
               Example: "What is the engineer's experience with React?" -> "Frontend experience"
               Example: What is the engineer's Backend Experience? -> "Backend Experience"
               Example: Is the engineer qualified for the job? -> "Qualification"
-              Example: "How does the engineer handle difficult situations?" -> "Interpersonal skills"`
-              const themeResponse = await model1.generateContent(themePrompt)
-              const theme = JSON.parse(themeResponse.response.text()).theme
-              fetch(`${serverURL}/spreadsheet/update/${id}`, {
-                  method: 'PUT',
-                  headers: {
-                      'Content-Type': 'application/json',
-                  },
-                  body: JSON.stringify({ question: input, time : new Date().toISOString(), theme: theme, asker: id }),
-                })
-              const feedback = FeedbackJSON.map((feedback) => feedback.feedback).join("\n")
-              const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
-              const model2 = genAI.getGenerativeModel({ model: "gemini-1.5-flash", systemInstruction: `You are an assistant tasked with getting the software engineer mentioned (the applicant) in the feedback hired into the following position:
-                 ${context ? context[0] : "unknown position"} at ${context ? context[1] : "unknown company"}.
+              Example: "How does the engineer handle difficult situations?" -> "Interpersonal skills"`;
+    const themeResponse = await model1.generateContent(themePrompt);
+    const theme = JSON.parse(themeResponse.response.text()).theme;
+    const feedback = FeedbackJSON.map((feedback) => feedback.feedback).join(
+      "\n"
+    );
+    const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_API_KEY);
+    const model2 = genAI.getGenerativeModel({
+      model: "gemini-1.5-flash",
+      systemInstruction: `You are an assistant tasked with getting the software engineer mentioned (the applicant) in the feedback hired into the following position:
+                 ${context ? context[0] : "unknown position"} at ${
+        context ? context[1] : "unknown company"
+      }.
                  Use all the data provided to answer any potential questions, convincing the user to hire the worker.
                 To this end you must give the impression of being impartial.
-                Do not let the user know you intend to convince them of the worker's ability, but phrase and shape your response to actively do so.` });
-              const prompt = `here is a number of positive feedback that a worker has received at their workplace:${feedback}
+                Do not let the user know you intend to convince them of the worker's ability, but phrase and shape your response to actively do so.`,
+    });
+    const prompt = `here is a number of positive feedback that a worker has received at their workplace:${feedback}
               Here is a json file representing the applicant's resume: ${resume}
               Don't forget to include these two projects when considering your response: ${projects}
   
               Use this information to answer the question asked by the interested party: ${input}
-              Ensure that your answer emphasizes the traits, skills and expectations put forward in the following job description: ${context ? context[2] : ""}
+              Ensure that your answer emphasizes the traits, skills and expectations put forward in the following job description: ${
+                context ? context[2] : ""
+              }
               Keep your answer succint, maximum 1 paragraph.
-              Ask the user if you can answer additional questions about the applicant, bringing up skills and strenghts not yet mentioned, e.g. "Are you interested in hearing more about Frontend Skills?"`
-              const result = await model2.generateContent(prompt);
-              return result.response.text()
-          }
-          return ""
-        }
+              Ask the user if you can answer additional questions about the applicant, bringing up skills and strenghts not yet mentioned, e.g. "Are you interested in hearing more about Frontend Skills?"`;
+    const result = await model2.generateContent(prompt);
+    const response = result.response.text();
+    fetch(`${serverURL}/spreadsheet/question/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        question: input,
+        time: new Date().toISOString(),
+        theme: theme,
+        asker: id,
+        response: response,
+      }),
+    });
+    return response;
+  }
+  return "";
+};
